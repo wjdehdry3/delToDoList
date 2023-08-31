eb8788183853d9b1287c018a29896ef5c92eeb2b
wjdehdry3 <wjdehdry3@gmail.com>
2023-09-01 08:59:28 +0900

    Initial Commit

---
+++ DelToDoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DelToDoList
+//
+//  Created by 정동교 on 2023/09/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ DelToDoListUITests/DelToDoListUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  DelToDoListUITestsLaunchTests.swift
+//  DelToDoListUITests
+//
+//  Created by 정동교 on 2023/09/01.
+//
+
+import XCTest
+
+final class DelToDoListUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
